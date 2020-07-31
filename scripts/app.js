464d8ea33999eb5a3f4afaac21d85f1f0ee88253
inanutsuki <d.buzalja@gmail.com>
2020-07-31 11:50:44 +0200

    good version

--- scripts/app.js
+++ scripts/app.js
@@ -10,12 +10,6 @@ const tempMinMax = document.querySelector('.temp_min-max');
 const windSpd = document.querySelector('.wind_spd');
 const windDir = document.querySelector('.wind_dir');
 
-const dayOneLink = document.querySelector('.link')
-const dayTwoLink = document.querySelector('a[href="#day_two"]')
-const dayThreeLink = document.querySelector('a[href="#day_three"]')
-const dayFourLink = document.querySelector('a[href="#day_four"]')
-const dayFiveLink = document.querySelector('a[href="#day_five"]')
-
 let $cityName;
 let $currentCondition;
 let $currentConditionIcon;
@@ -26,82 +20,38 @@ let $humidity;
 let $windDir;
 let $windSpd;
 
-/**day one**/
-let $longDayOne;
-let $shortDayOne;
-let $tempMinDayOne;
-let $tempMaxDayOne;
-let $currentConditionIconDayOne;
-/**day two**/
-let $longDayTwo;
-let $shortDayTwo;
-let $tempMinDayTwo;
-let $tempMaxDayTwo;
-let $currentConditionIconDayTwo;
-/**day three**/
-let $longDayThree;
-let $shortDayThree;
-let $tempMinDayThree;
-let $tempMaxDayThree;
-let $currentConditionIconDayThree;
-/**day four**/
-let $longDayFour;
-let $shortDayFour;
-let $tempMinDayFour;
-let $tempMaxDayFour;
-let $currentConditionIconDayFour;
-/**day five**/
-let $longDayFive;
-let $shortDayFive;
-let $tempMinDayFive;
-let $tempMaxDayFive;
-let $currentConditionIconDayFive;
+let $day = [];
+let $forecastData = [];
 
 
 let $city;
 let $url;
 
-/**ne les reconnait plus comme etant des variables ??? */
-
-// function putInfoIn() {
-//     let number = [
-//         One
-//     ]
-//     for (let i = 0; i < number.length; i++) {
-
-//         let day = [
-//             // $shortDayOne,
-//             // $currentConditionIconDayOne,
-//             // $tempMinDayOne,
-//             // $tempMaxDayOne
-//         ]
-//         let a = $shortDay + number[i, 0];
-//         let b = $currentConditionIconDay + number[i, 0];
-//         let c = $tempMinDay + number[i, 0];
-//         let d = $tempMaxDay + number[i, 0];
-//         day.push(a, b, c, d)
-//         for (let i = 0; i < day.length; i++) {
-//             console.log(day[i]);
-//             dayOneLink.innerHTML = '<div>' + day[i, 0] + '</div><img src="' + day[i, 1] + '"></img><div>' + day[i, 2] + '°C / ' + day[i, 3] + '°C</div>';
-//         }
-//     }
-// }
-
 function putInfoIn() {
-
-    let day = [
-        $shortDayOne,
-        $currentConditionIconDayOne,
-        $tempMinDayOne,
-        $tempMaxDayOne
-    ]
-    for (let i = 0; i < day.length; i++) {
-        console.log(day[i]);
-        dayOneLink.innerHTML = '<div>' + day[i, 0] + '</div><img src="' + day[i, 1] + '"></img><div>' + day[i, 2] + '°C / ' + day[i, 3] + '°C</div>';
+    for (let i = 1; i <= 5; ++i) {
+        const dayNumb = {
+            day: document.querySelector('.link' + i)
+        }
+        $day.push(dayNumb);
+        for (let i = 0; i < $day.length; ++i) {
+            $day[i].day.innerHTML = `<div>${$forecastData[i].shortDay}</div>
+                                <img src="${$forecastData[i].currentConditionIconDay}"></img>
+                                <div>${$forecastData[i].tempMaxDay}°C / ${$forecastData[i].tempMinDay}°C</div>`;
+        }
     }
 }
 
 
+if ("geolocation" in navigator) {
+    
+    console.log('ok');
+  } else {
+    console.log('pas ok');
+  }
+  navigator.geolocation.getCurrentPosition(function(position) {
+    posLat = position.coords.latitude, posLong = position.coords.longitude;
+    console.log(posLat, posLong);
+  });
 
 function setInfos() {
     city.innerHTML = $cityName;
@@ -110,9 +60,10 @@ function setInfos() {
     temperature.innerHTML = `Il fait ${$temperature}°C`;
     hour.innerHTML = `Prévision pour ${$hour}h`;
     humidity.innerHTML = `Il y a ${$humidity}% d'humidité`;
-    tempMinMax.innerHTML = `${$tempMaxDayOne}°C / ${$tempMinDayOne}°C`;
+    tempMinMax.innerHTML = `${$forecastData[0].tempMaxDay}°C / ${$forecastData[0].tempMinDay}°C`;
     windSpd.innerHTML = `La vitesse du vent est de ${$windSpd}km/h`;
     windDir.innerHTML = `La direction du vent est ${$windDir}`;
+
     putInfoIn();
     return;
 }
@@ -123,7 +74,9 @@ function actionFetch() {
             return response.json();
         })
         .then(function (response) {
+
             console.log(response)
+
             $cityName = response.city_info.name;
             $currentCondition = response.current_condition.condition;
             $currentConditionIcon = response.current_condition.icon_big;
@@ -134,36 +87,15 @@ function actionFetch() {
             $temperature = response.current_condition.tmp;
             $date = response.current_condition.date;
 
-            /**day one**/
-            $shortDayOne = response.fcst_day_0.day_short;
-            $currentConditionIconDayOne = response.fcst_day_0.icon;
-            $tempMaxDayOne = response.fcst_day_0.tmax;
-            $tempMinDayOne = response.fcst_day_0.tmin;
-            $longDayOne = response.fcst_day_0.day_long;
-            /**day two**/
-            $shortDayTwo = response.fcst_day_1.day_short;
-            $currentConditionIconDayTwo = response.fcst_day_1.icon;
-            $tempMaxDayTwo = response.fcst_day_1.tmax;
-            $tempMinDayTwo = response.fcst_day_1.tmin;
-            $longDayTwo = response.fcst_day_1.day_long;
-            /**day three**/
-            $shortDayThree = response.fcst_day_2.day_short;
-            $currentConditionIconDayThree = response.fcst_day_2.icon;
-            $tempMaxDayThree = response.fcst_day_2.tmax;
-            $tempMinDayThree = response.fcst_day_2.tmin;
-            $longDayThree = response.fcst_day_2.day_long;
-            /**day four**/
-            $shortDayFour = response.fcst_day_3.day_short;
-            $currentConditionIconDayFour = response.fcst_day_3.icon;
-            $tempMaxDayFour = response.fcst_day_3.tmax;
-            $tempMinDayFour = response.fcst_day_3.tmin;
-            $longDayFour = response.fcst_day_3.day_long;
-            /**day five**/
-            $shortDayFive = response.fcst_day_4.day_short;
-            $currentConditionIconDayFive = response.fcst_day_4.icon;
-            $tempMaxDayFive = response.fcst_day_4.tmax;
-            $tempMinDayFive = response.fcst_day_4.tmin;
-            $longDayFive = response.fcst_day_4.day_long;
+            for (let i = 0; i < 5; ++i) {
+                const dayData = {
+                    shortDay: response["fcst_day_" + i].day_short,
+                    currentConditionIconDay: response["fcst_day_" + i].icon,
+                    tempMinDay: response["fcst_day_" + i].tmin,
+                    tempMaxDay: response["fcst_day_" + i].tmax
+                }
+                $forecastData.push(dayData);
+            }
 
             setInfos();
 
@@ -222,7 +154,7 @@ function setClassActive(link) {
     tabsWrapper.querySelector(link.getAttribute('href')).classList.add('active');
 }
 
-for (let i = 0; i < tabs.length; i++) {
+for (let i = 0; i < tabs.length; ++i) {
     tabs[i].addEventListener('mouseover', function (event) {
         setClassActive(this);
     })
